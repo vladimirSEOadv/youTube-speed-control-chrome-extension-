bde1eab999a22d6a664db352dff089ff537ec7c6
vladimirSEOadv <vladimirSEOadv@gmail>
2021-11-06 00:05:16 +0300

    refactoring

--- contentScript.js
+++ contentScript.js
@@ -1,69 +1,81 @@
-function mainfunc () {
-	var videoSpeed = document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate;
-	var counter = 0.25;
+function mainfunc() {
+	const yPlayer = document.querySelector('.video-stream.html5-main-video')
+	let videoSpeed = yPlayer.playbackRate;
+	// let videoSpeed = localStorage.getItem('youTubeVideoSpeed') === null ? yPlayer.playbackRate : localStorage.getItem('youTubeVideoSpeed');
+	const counter = 0.25;
 
-	var NewButton1 = document.createElement("button");
-		NewButton1.style.backgroundColor = 'red';
-		NewButton1.style.margin = "10px";
-		NewButton1.style.height = "25px";
-		NewButton1.style.width = "55px";
-		NewButton1.style.color = "white";
-		NewButton1.classList.add('video-control1');
-		NewButton1.textContent = "slow";
+	const divSpeedCounter = document.createElement("div");
+	divSpeedCounter.style.margin = "10px";
+	divSpeedCounter.style.height = "25px";
+	divSpeedCounter.style.width = "55px";
+	divSpeedCounter.style.fontSize = "20px"
+	divSpeedCounter.textContent = document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate;
+	divSpeedCounter.classList.add('video-counter');
 
-		NewButton1.addEventListener('click', function (event) {
-			document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate = (videoSpeed-counter);
-		  videoSpeed = document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate;
-		  divSpeedCounter.innerHTML = videoSpeed;
-		});
+	document.getElementById(`flex`).appendChild(divSpeedCounter);
 
-	document.getElementById(`flex`).appendChild(NewButton1);
+	function elementCreator(elementType, backgroundColor, elemText, elemClass) {
+		element = document.createElement(elementType);
+		element.classList.add(elemClass);
+		element.textContent = elemText;
+		element.style.backgroundColor = backgroundColor;
+		element.style.cssText += 'margin: 10px; height: 25px; width: 55px; color: white;';
+		return element
+	}
 
+	const ButtonSlow = elementCreator("button", "red", "slow", "video-control1")
 
-	var NewButton2 = document.createElement("button");
-		NewButton2.style.backgroundColor = 'black';
-		NewButton2.style.margin = "10px";
-		NewButton2.style.height = "25px";
-		NewButton2.style.width = "55px";
-		NewButton2.style.color = "white";
-		NewButton2.textContent = "normal";
-		NewButton2.classList.add('video-control2');
-		
-		NewButton2.addEventListener('click', function (event) {
-			document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate = 1.0;
-			videoSpeed = document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate;
-			divSpeedCounter.innerHTML = videoSpeed;
-		  	
-		});
-
-	document.getElementById(`flex`).appendChild(NewButton2);
-
-	var NewButton3 = document.createElement("button");
-		NewButton3.style.backgroundColor = 'green';
-		NewButton3.style.margin = "10px";
-		NewButton3.style.height = "25px";
-		NewButton3.style.width = "55px";
-		NewButton3.style.color = "white";
-		NewButton3.textContent = "fast";
-		NewButton3.classList.add('video-control3');
-		
-		NewButton3.addEventListener('click', function (event) {
-			document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate = (videoSpeed+counter)
-			videoSpeed = document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate;			
-			divSpeedCounter.innerHTML = videoSpeed;
-
+	ButtonSlow.addEventListener('click', function (event) {
+		changeVideoSpeed('down')
 	});
 
-	document.getElementById(`flex`).appendChild(NewButton3);
+	document.getElementById(`flex`).appendChild(ButtonSlow);
 
-	var divSpeedCounter = document.createElement("div");
-		divSpeedCounter.style.margin = "10px";
-		divSpeedCounter.style.height = "25px";
-		divSpeedCounter.style.width = "55px";
-		divSpeedCounter.style.fontSize = "20px"
-		divSpeedCounter.textContent = document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate;
-		divSpeedCounter.classList.add('video-counter');
+	const ButtonNormal = elementCreator("button", "black", "normal", "video-control2")
 
-		document.getElementById(`flex`).appendChild(divSpeedCounter);
+	ButtonNormal.addEventListener('click', function (event) {
+		changeVideoSpeed('normal')
+	});
+
+	document.getElementById(`flex`).appendChild(ButtonNormal);
+
+	const ButtonFast = elementCreator("button", "green", "fast", "video-control3")
+
+	ButtonFast.addEventListener('click', function (event) {
+		changeVideoSpeed('up')
+	});
+	document.getElementById(`flex`).appendChild(ButtonFast);
+
+	if (localStorage.getItem('youTubeVideoSpeed')) { // Иницилизация, чтение скорости из локального хранилища
+		videoSpeed = localStorage.getItem('youTubeVideoSpeed')
+		divSpeedCounter.innerHTML = videoSpeed;
+	};
+
+	function changeVideoSpeed(upOrDown) {
+		switch (upOrDown) {
+			case 'up':
+				videoSpeed = (videoSpeed + counter);
+				break
+			case 'down':
+				videoSpeed = (videoSpeed - counter);
+				break
+			case 'normal':
+				videoSpeed = 1.0;
+				break
+		}
+		yPlayer.playbackRate = videoSpeed;
+		localStorage.setItem('youTubeVideoSpeed', videoSpeed);
+		divSpeedCounter.innerHTML = videoSpeed;
+	}
+
+	function autoUpdateVideoSpeed() {
+		if (document.querySelector('#movie_player').classList.contains('playing-mode') && document.querySelector('.video-stream.html5-main-video').playbackRate != videoSpeed) {
+			document.querySelector('.video-stream.html5-main-video').playbackRate = videoSpeed;
+			divSpeedCounter.innerHTML = videoSpeed;
+			localStorage.setItem('youTubeVideoSpeed', videoSpeed);
+		}
+	}
+	setInterval(autoUpdateVideoSpeed, 5000)
 };
-setTimeout(mainfunc,3000);
+// window.onload = mainfunc()
+setTimeout(mainfunc, 3000)
