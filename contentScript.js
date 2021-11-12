09516cf3adf8dc1a33bfc258c593acb232f481c8
vladimirSEOadv <vladimirSEOadv@gmail>
2021-11-13 01:38:33 +0300

    refactoring EventListener control speed buttons

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,5 @@
 function mainfunc() {
-	const yPlayer = document.querySelector('.video-stream.html5-main-video')
-	// let videoSpeed = yPlayer.playbackRate;
+	const yPlayer = document.querySelector('.video-stream.html5-main-video');
 	let videoSpeed = localStorage.getItem('youTubeVideoSpeed') != null ? Number(localStorage.getItem('youTubeVideoSpeed')) : yPlayer.playbackRate;
 	const counter = 0.25;
 
@@ -16,61 +15,43 @@ function mainfunc() {
 
 	document.getElementById(`flex`).appendChild(divSpeedCounter);
 
-	function elementCreator(elementType, backgroundColor, elemText, elemClass) { //Конструктор кнопок
+	function elementCreator(elementType, backgroundColor, elemText, elemId) { //Конструктор кнопок
 		element = document.createElement(elementType);
-		element.classList.add(elemClass);
+		element.id = elemId;
 		element.textContent = elemText;
 		element.style.backgroundColor = backgroundColor;
 		element.style.cssText += 'margin: 10px; height: 25px; width: 55px; color: white;';
-		document.getElementById(`flex`).appendChild(element)
+		document.getElementById(`flex`).appendChild(element);
 		return element
 	}
 
-	const ButtonSlow = elementCreator("button", "red", "slow", "video-control1")
-
-	ButtonSlow.addEventListener('click', function (event) {
-		changeVideoSpeed('down')
+	document.getElementById(`flex`).addEventListener('click', (event) => { // Обработчик событий кликов
+		switch (event.target.id) {
+			case 'fast-button-for-yt-video':
+				videoSpeed = (videoSpeed + counter);
+				break
+			case 'normal-button-for-yt-video':
+				videoSpeed = 1.0;
+				break
+			case 'slow-button-for-yt-video':
+				videoSpeed = (videoSpeed - counter);
+				break
+		};
+		yPlayer.playbackRate = videoSpeed;
+		localStorage.setItem('youTubeVideoSpeed', videoSpeed);
+		divSpeedCounter.innerHTML = videoSpeed;
 	});
 
-	// document.getElementById(`flex`).appendChild(ButtonSlow);
-
-	const ButtonNormal = elementCreator("button", "black", "normal", "video-control2")
-
-	ButtonNormal.addEventListener('click', function (event) {
-		changeVideoSpeed('normal');
-	});
-
-	// document.getElementById(`flex`).appendChild(ButtonNormal);
-
-	const ButtonFast = elementCreator("button", "green", "fast", "video-control3")
-
-	ButtonFast.addEventListener('click', function (event) {
-		changeVideoSpeed('up');
-	})
+	const ButtonSlow = elementCreator("button", "red", "slow", "slow-button-for-yt-video");
+	const ButtonNormal = elementCreator("button", "black", "normal", "normal-button-for-yt-video");
+	const ButtonFast = elementCreator("button", "green", "fast", "fast-button-for-yt-video");
 
 	if (localStorage.getItem('youTubeVideoSpeed')) { // Иницилизация, чтение скорости из локального хранилища
 		videoSpeed = Number(localStorage.getItem('youTubeVideoSpeed'));
 		divSpeedCounter.innerHTML = videoSpeed;
 	};
 
-	function changeVideoSpeed(upOrDown) {
-		switch (upOrDown) {
-			case 'up':
-				videoSpeed = (videoSpeed + counter);
-				break
-			case 'down':
-				videoSpeed = (videoSpeed - counter);
-				break
-			case 'normal':
-				videoSpeed = 1.0;
-				break
-		}
-		yPlayer.playbackRate = videoSpeed;
-		localStorage.setItem('youTubeVideoSpeed', videoSpeed);
-		divSpeedCounter.innerHTML = videoSpeed;
-	}
-
-	function autoUpdateVideoSpeed() {
+	function autoUpdateVideoSpeed() { // Автообновление скорости воспроизведения
 		if (document.querySelector('#movie_player').classList.contains('playing-mode') && document.querySelector('.video-stream.html5-main-video').playbackRate != videoSpeed) {
 			document.querySelector('.video-stream.html5-main-video').playbackRate = videoSpeed;
 			divSpeedCounter.innerHTML = videoSpeed;
